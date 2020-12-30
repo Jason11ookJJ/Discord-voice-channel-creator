191edfb07eee07f9cdca78b982a7600669592508
Jason11ookJJ <jasonho1308@gmail.com>
2020-12-30 22:59:32 +0800

    A DM will send to the owner when ext. change

--- cogs/owner.py
+++ cogs/owner.py
@@ -14,18 +14,23 @@ class owner(commands.Cog):
     @commands.is_owner()
     async def load(self, ctx, extension):
         self.bot.load_extension(f'cogs.{extension}')
-        await ctx.channel.send(f"{extension} loaded")
+        await ctx.author.send(f"{extension} loaded")
+        print(f"Extension: {extension} loaded")
 
     @commands.command(brief='Unload extension', description='Unload extension')
     @commands.is_owner()
     async def unload(self, ctx, extension):
         self.bot.unload_extension(f'cogs.{extension}')
+        await ctx.author.send(f"{extension} unloaded")
+        print(f"Extension: {extension} unloaded")
 
     @commands.command(brief='Reload extension', description='Reload extension')
     @commands.is_owner()
     async def reload(self, ctx, extension):
         self.bot.unload_extension(f'cogs.{extension}')
         self.bot.load_extension(f'cogs.{extension}')
+        await ctx.author.send(f"{extension} reloaded")
+        print(f"Extension: {extension} unreloaded")
 
 def setup(bot):
     bot.add_cog(owner(bot))
