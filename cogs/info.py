50bb053673e70a40dbfcf7b42c76fef2866b55e0
Jason11ookJJ <jasonh1308@gmail.com>
2021-01-05 10:10:41 +0800

    Update the change log to Pre release v0.1.5

--- cogs/info.py
+++ cogs/info.py
@@ -10,17 +10,14 @@ class info(commands.Cog):
     async def on_ready(self):
         print("Info cog is ready")
 
-    @commands.command(brief='Shows change log', description='Shows current version and change log')    
+    @commands.command(brief='Shows change log of newest version', description='Shows current version and change log')    
     async def change_log(self, ctx):
         channel = ctx.channel
         embedVar = discord.Embed(title="Change Log", description="", color=0x0ae0fc)
-        embedVar.add_field(name="Pre release v0.1.4", value='''
+        embedVar.add_field(name="Pre release v0.1.5", value='''
                 Added feature:
-                1. Now user can @people @role and also give custom name to channel
-                2. create temporary channel without @people or @role
-
-                New command: 
-                vc create @people/@role/text''',
+                1. the bot will alert the user when a voice channel is deleted
+                ''',
                 inline=False)
         await channel.send(embed=embedVar)
 
