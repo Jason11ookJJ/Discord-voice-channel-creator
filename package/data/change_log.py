1e4b065da6883f2047eca488d8d801988b553bac
Jason11ookJJ <jasonh1308@gmail.com>
2021-01-21 15:12:46 +0800

    change from json to python

---
+++ package/data/change_log.py
@@ -0,0 +1,41 @@
+change_log = {
+    "0.1.6": """
+    Added feature:
+    ✨ Your server will become prettier, all message from the bot is now embedded.
+    "Change log" command has changed, now you can access all version change log instead of one.
+    
+    Bug fix: 
+    Voice channel naming problem 
+
+    Command change:
+    vc change_log [version number] (default is the latest version)""",
+
+
+    "0.1.5": """
+    Added feature: 
+    1. the bot will alert the user when a voice channel is deleted""",
+
+
+    "0.1.4": """
+    Added feature:
+    1. Now user can @people @role and also give custom name to channel 
+    2. create temporary channel without @people or @role 
+
+    New command: 
+    vc create @people/@role/text""",
+
+
+    "0.1.3": """
+    Added feature: 
+     1. Only role member can create role channel""",
+
+
+    "0.1.2": """
+    Fix bug: 
+     Channel will not be closed after bot reset""",
+
+
+    "0.1.1": """
+    Fix bug: 
+     channel will not be closed after bot reset"""
+}
