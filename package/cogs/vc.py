f0c9e991ab4f9ff9a960adae6fd04e4781f72c59
Jason11ookJJ <jasonho1308@gmail.com>
2021-02-05 17:23:10 +0800

    Added "vc text" and rename some functions

--- package/cogs/vc.py
+++ package/cogs/vc.py
@@ -26,7 +26,7 @@ class Vc(commands.Cog, name="Voice Channel"):
                                   view_channel=True,
                                   embed_links=True)
     async def create(self, ctx):
-        i = await create_channel(ctx)
+        i = await create_voice(ctx)
         if i:
             msg = i.get("msg")
             new_channel = i.get("new_channel")
@@ -40,13 +40,13 @@ class Vc(commands.Cog, name="Voice Channel"):
                                     """, color=0x0ae0fc)
             response_msg = await msg.channel.send(embed=embed_var)
 
-            db.save_channel(new_channel.id, msg.channel.id, response_msg.id)
+            db.save_voice_channel(new_channel.id, msg.channel.id, response_msg.id)
 
             print(f"{current_time()} VC: a channel is created (created: 1, deleted: 0)")
             save(self, 1, 0)
 
-    @commands.command(brief='Create a voice channel',
-                      description='Create a voice channel that only certain role can speak')
+    @commands.command(brief='Create a private voice channel',
+                      description='Create a private voice channel that only certain role can speak and hear')
     @commands.bot_has_permissions(read_messages=True,
                                   manage_messages=True,
                                   manage_channels=True,
@@ -57,7 +57,7 @@ class Vc(commands.Cog, name="Voice Channel"):
         msg = ctx.message
         mention = msg.role_mentions + msg.mentions
         if mention:
-            i = await create_channel(ctx)
+            i = await create_voice(ctx)
             if i:
                 msg = i.get("msg")
                 new_channel = i.get("new_channel")
@@ -77,7 +77,7 @@ class Vc(commands.Cog, name="Voice Channel"):
                                             """, color=0x178fff)
                 response_msg = await msg.channel.send(embed=embed_var)
 
-                db.save_channel(new_channel.id, msg.channel.id, response_msg.id)
+                db.save_voice_channel(new_channel.id, msg.channel.id, response_msg.id)
 
                 print(f"{current_time()} VC: a private channel is created (created: 1, deleted: 0)")
                 save(self, 1, 0)
@@ -89,17 +89,50 @@ class Vc(commands.Cog, name="Voice Channel"):
             await msg.channel.send(embed=embed_var)
             return
 
+    @commands.command(brief='Create a voice and text channel',
+                      description='Create a voice channel that only certain role can speak')
+    @commands.bot_has_permissions(read_messages=True,
+                                  manage_messages=True,
+                                  manage_channels=True,
+                                  send_messages=True,
+                                  view_channel=True,
+                                  embed_links=True)
+    async def text(self, ctx):
+        i = await create_voice(ctx)
+        q = await create_text(ctx)
+        if i and q:
+            msg = i.get("msg")
+            new_channel = i.get("new_channel")
+            embed_var = discord.Embed(title="", description=f"""Vcc text and voice
+                                                                Name: {i.get("channel_name")}
+                                                                Speaker: {i.get("mention_name")}
+                                                                Creator: {msg.author.mention}
+                                    
+                                                                Started: {current_time()}
+                                                                """, color=0x0ae0fc)
+            response_msg = await msg.channel.send(embed=embed_var)
+            db.save_voice_channel(new_channel.id, msg.channel.id, response_msg.id)
+            db.save_text_channel(new_channel.id, q.get("new_channel").id)
+            print(f"{current_time()} VC: a channel is created (created: 1, deleted: 0)")
+            save(self, 1, 0)
+
     @commands.Cog.listener()
     async def on_voice_state_update(self, client, before, after):
         if before.channel is not None:
-            result = db.get_all_channel(before.channel.id)
+            result = db.get_channel(before.channel.id)
             if result:
                 if not before.channel.members:
                     await before.channel.delete()
-                    db.delete_channel(before.channel.id)
 
-                    msg_channel = self.bot.get_channel(result[0][1])
-                    response_msg = await msg_channel.fetch_message(result[0][2])
+                    # delete text channel
+                    tx_channel_id = result.get("tx_channel_id")
+                    if tx_channel_id:
+                        tx_channel = self.bot.get_channel(tx_channel_id)
+                        await tx_channel.delete()
+
+                    # edit message
+                    msg_channel = self.bot.get_channel(result.get("msg_channel_id"))
+                    response_msg = await msg_channel.fetch_message(result.get("response_msg_id"))
                     embeds = response_msg.embeds
                     if embeds:
                         description = embeds[0].description
@@ -110,11 +143,13 @@ class Vc(commands.Cog, name="Voice Channel"):
                         """, color=0x129eb0)
                         await response_msg.edit(embed=embed_var)
 
+                    db.delete_channel(before.channel.id)
+
                     print(f"{current_time()} VC: a channel is deleted (created: 0, deleted: 1)")
                     save(self, 0, 1)
 
 
-async def create_channel(ctx):
+async def create_voice(ctx):
     msg = ctx.message
     mention = msg.role_mentions + msg.mentions
     mention_list = []
@@ -122,7 +157,7 @@ async def create_channel(ctx):
     if mention:  # @role
         if check_in_role(msg.author.id, msg.role_mentions):
             # create channel
-            channel_name = re.sub('[@]', '', msg.clean_content[11:])
+            channel_name = get_channel_name(msg)
             new_channel = await msg.channel.category.create_voice_channel(channel_name)
 
             # setting permission
@@ -153,6 +188,49 @@ async def create_channel(ctx):
 
     return {"msg": msg, "new_channel": new_channel, "mention": mention, "channel_name": channel_name,
             "mention_name": mention_name}
+
+
+async def create_text(ctx):
+    msg = ctx.message
+    mention = msg.role_mentions + msg.mentions
+
+    if mention:  # @role
+        if check_in_role(msg.author.id, msg.role_mentions):
+            # create channel
+            channel_name = get_channel_name(msg)
+            new_channel = await msg.channel.category.create_text_channel(channel_name)
+
+            # setting permission
+            for i in mention:
+                await new_channel.set_permissions(i, send_messages=True)
+
+            await new_channel.set_permissions(ctx.guild.roles[0], send_messages=False)
+        else:  # not member of @role
+            embed_var = discord.Embed(title="", description=f"""
+                        {msg.author.mention}
+                        You are not a member of that role,
+                        Please ping another role or people
+                        """, color=0xff0f0f)
+            await msg.channel.send(embed=embed_var)
+            return
+    else:  # only text
+        channel_name = msg.clean_content[10:]
+        if channel_name == "":
+            channel_name = "created by Vcc"
+        new_channel = await msg.channel.category.create_text_channel(channel_name)
+
+    return {"new_channel": new_channel}
+
+
+def get_channel_name(msg):
+    channel_name = " ".join(msg.clean_content.split(" ")[2:])
+    if len(channel_name) > 100:
+        embed_var = discord.Embed(title="", description=f"""
+                                            {msg.author.mention}
+                                            Channel name is too long (Max: 100 characters)
+                                            """, color=0xff0f0f)
+        await msg.channel.send(embed=embed_var)
+    return channel_name
 
 
 def save(self, created, deleted):
--- package/data/databaseDeo.py
+++ package/data/databaseDeo.py
@@ -6,23 +6,50 @@ import os
 def connect():
     conn = sqlite3.connect('data.db')
     db = conn.cursor()
-    db.execute('''CREATE TABLE IF NOT EXISTS vc_channel(channel_id int, msg_channel int, response_msg_id int)''')
     db.execute(
-        '''CREATE TABLE IF NOT EXISTS full_statistic(Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP, server_in_use int, 
-        vc_created int, vc_deleted int)''')
+        '''CREATE TABLE IF NOT EXISTS vc_channel(
+        channel_id int, 
+        msg_channel_id int, 
+        response_msg_id int)''')
     db.execute(
-        '''CREATE TABLE IF NOT EXISTS error(Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP, author String, msg String, 
+        '''CREATE TABLE IF NOT EXISTS text_channel(
+        voice_channel_id int, 
+        channel_id int, 
+        FOREIGN KEY (voice_channel_id) REFERENCES vc_channel(channel_id) ON DELETE CASCADE)''')
+    db.execute(
+        '''CREATE TABLE IF NOT EXISTS full_statistic(
+        Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+         server_in_use int, 
+         vc_created int, 
+         vc_deleted int)''')
+    db.execute(
+        '''CREATE TABLE IF NOT EXISTS error(
+        Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP, 
+        author String, 
+        msg String, 
         error String)''')
     conn.commit()
     return conn
 
 
-def save_channel(new_channel_id, msg_channel_id, response_msg_id):
+def save_voice_channel(new_channel_id, msg_channel_id, response_msg_id):
     try:
         conn = connect()
         db = conn.cursor()
-        db.execute("INSERT INTO vc_channel(channel_id, msg_channel, response_msg_id) VALUES (?, ?, ?)",
+        db.execute("INSERT INTO vc_channel(channel_id, msg_channel_id, response_msg_id) VALUES (?, ?, ?)",
                    (new_channel_id, msg_channel_id, response_msg_id))
+        conn.commit()
+        conn.close()
+    except Exception as e:
+        print(f'''{current_time()} Error: DB ({e})''')
+
+
+def save_text_channel(voice_channel_id, text_channel_id):
+    try:
+        conn = connect()
+        db = conn.cursor()
+        db.execute("INSERT INTO text_channel(voice_channel_id, channel_id) VALUES (?, ?)",
+                   (voice_channel_id, text_channel_id))
         conn.commit()
         conn.close()
     except Exception as e:
@@ -54,14 +81,23 @@ def stats_save(server_count, created, deleted):
         print(f'''{current_time()} Error: DB ({e})''')
 
 
-def get_all_channel(before_channel_id):
+def get_channel(before_channel_id):
     try:
         conn = connect()
         db = conn.cursor()
-        result = db.execute("SELECT * FROM vc_channel WHERE channel_id = (?)", [before_channel_id]).fetchall()
-        conn.commit()
+        result = db.execute("""SELECT
+                                tc.channel_id AS 'tx_channel_id',
+                                vc.msg_channel_id,
+                                vc.response_msg_id
+                            FROM
+                                 vc_channel vc
+                            LEFT JOIN text_channel tc
+                            ON vc.channel_id = tc.voice_channel_id
+                            WHERE vc.channel_id = (?)""",
+                            [before_channel_id]).fetchall()
         conn.close()
-        return result
+        data = dict(zip([c[0] for c in db.description], result[0]))
+        return data
     except Exception as e:
         print(f'''{current_time()} Error: DB ({e})''')
 
