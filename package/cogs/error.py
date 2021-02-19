c13e6afb95e8378b8cee458ff3ad79aaaee716be
Jason11ookJJ <jasonho1308@gmail.com>
2021-02-19 18:40:31 +0800

    Catch error when channel number is maximum.

--- package/cogs/error.py
+++ package/cogs/error.py
@@ -17,25 +17,31 @@ class Error(commands.Cog, name="Voice Channel"):
     async def on_command_error(self, ctx, error):
         await ctx.message.add_reaction("🛑")
         if isinstance(error, BotMissingPermissions):  # checking which type of error it is
-            embed_var = discord.Embed(title="Sorry, an error occurred", description=f"""
+            embed_var = discord.Embed(title="Command error", description=f"""
                     {error} It seems that I don't have enough permission to do this, please sent a message to the 
                      server admin to fix this problem. Enter "vc permission" to check for the permission that I need. 
                     """, color=0xff0f0f)
             await ctx.send(embed=embed_var)
         elif isinstance(error, CommandNotFound):
-            embed_var = discord.Embed(title="Sorry, an error occurred", description=f"""
-                    {ctx.message.clean_content.split(" ")[1]} Command not found
-                    All commands are case-incentive, enter "vc help" for more 
+            embed_var = discord.Embed(title="Command error", description=f"""
+                    {str(error)}
+                    All commands are cAsE sEnSitIvE, enter "vc help" for more 
                     """, color=0xff0f0f)
             await ctx.send(embed=embed_var)
+            db.save_error(ctx, error)
+        elif error.original.code == 50035:
+            embed_var = discord.Embed(title="Command error", description=f"""
+                                Maximum number of channels in category reached (50)
+                                """, color=0xff0f0f)
+            await ctx.send(embed=embed_var)
         else:
-            embed_var = discord.Embed(title="Sorry, an unknown error occurred", description=f""" An unexpected error 
+            embed_var = discord.Embed(title="Unknown error", description=f""" An unexpected error 
             occurred, please report this issue on [GitHub](
             https://github.com/Jason11ookJJ/Discord-voice-channel-creator/issues) OR join the [support server](
             https://discord.gg/P5Fd4KXXEJ), we will fix it soon 
                     """, color=0xff0f0f)
             await ctx.send(embed=embed_var)
-        db.save_error(ctx, error)
+            db.save_error(ctx, error)
 
 
 def setup(bot):
