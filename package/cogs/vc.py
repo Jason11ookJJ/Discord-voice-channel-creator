1637f61eb463a6448c9e2aee0eb7354f9544b495
Jason11ookJJ <jasonho1308@gmail.com>
2021-03-03 22:14:10 +0800

    Fix check not define

--- package/cogs/vc.py
+++ package/cogs/vc.py
@@ -53,7 +53,8 @@ class Vc(commands.Cog, name="Voice Channel"):
                                   view_channel=True,
                                   embed_links=True)
     async def private(self, ctx):
-        if await check_in_role(ctx) == 0:
+        check = await check_in_role(ctx)
+        if check == 0:
             i = await create_voice(ctx)
             if i:
                 j = await create_text(ctx)
